5c15bdc6dc9f3a80623f1bf7c0fa470d4c414ce0
QuickFox43 <koalal1428@gmail.com>
2025-10-17 20:26:23 +0300

    other(schemas): add __init__.py for schemas

--- backend/src/schemas/__init__.py
+++ backend/src/schemas/__init__.py
@@ -1,5 +1,15 @@
 from src.schemas.base import BaseReadSchema, BaseSchema
-from src.schemas.skills import SkillCreate, SkillRead
+from src.schemas.skills import SkillBulkDelete, SkillCreate, SkillRead, SkillUpdate
 from src.schemas.user import UserCreate, UserRead, UserUpdate
 
-__all__ = ["BaseReadSchema", "BaseSchema", "SkillCreate", "SkillRead", "UserCreate", "UserRead", "UserUpdate"]
+__all__ = [
+    "BaseReadSchema",
+    "BaseSchema",
+    "SkillCreate",
+    "SkillRead",
+    "SkillUpdate",
+    "SkillBulkDelete",
+    "UserCreate",
+    "UserRead",
+    "UserUpdate",
+]
