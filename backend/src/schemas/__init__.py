1382ae1ab6c0b6c775980ec89f81cdb1dd1bb97d
QuickFox43 <koalal1428@gmail.com>
2025-10-17 17:43:53 +0300

    feat(schemas): add skills, user schemas

---
+++ backend/src/schemas/__init__.py
@@ -0,0 +1,5 @@
+from src.schemas.base import BaseReadSchema, BaseSchema
+from src.schemas.skills import SkillCreate, SkillRead
+from src.schemas.user import UserCreate, UserRead, UserUpdate
+
+__all__ = ["BaseReadSchema", "BaseSchema", "SkillCreate", "SkillRead", "UserCreate", "UserRead", "UserUpdate"]
--- backend/src/schemas/skills.py
+++ backend/src/schemas/skills.py
@@ -1,13 +1,22 @@
-from src.schemas.base import BaseSchema
+from typing import TYPE_CHECKING
+
 from src.enums.skill_type import SkillType
+from src.schemas.base import BaseSchema
+
+if TYPE_CHECKING:
+    from src.schemas.user import UserRead
+
 
 class SkillCreate(BaseSchema):
     type: SkillType
     name: str
     description: str | None = None
+    user_id: int
+
 
 class SkillRead(BaseSchema):
     id: int
     type: SkillType
     name: str
     description: str | None = None
+    user: "UserRead"
