a534a29593b0808b972b71250040054b1da41658
QuickFox43 <koalal1428@gmail.com>
2025-10-17 15:59:54 +0300

    feat(models): add skill model

---
+++ src/models/skills.py
@@ -0,0 +1,20 @@
+from typing import TYPE_CHECKING
+
+from sqlalchemy import String, Enum, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from src.models.base import Base
+from src.enums.skill_type import SkillType  
+
+if TYPE_CHECKING:
+    from src.models.user import User
+
+class Skill(Base):
+    __tablename__ = "skills"
+
+    type: Mapped[SkillType] = mapped_column(Enum(SkillType))
+    name: Mapped[str] = mapped_column(String(255), index=True)
+    description: Mapped[str | None] = mapped_column(nullable=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
+
+    user: Mapped["User"] = relationship(back_populates="skills")
