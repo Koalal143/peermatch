8c9928130ee58a56e08f712b804d156397ad23e5
QuickFox43 <koalal1428@gmail.com>
2025-10-18 00:17:39 +0300

    feat(repo): improve vector search

--- backend/src/repositories/vector_search.py
+++ backend/src/repositories/vector_search.py
@@ -4,6 +4,8 @@ from qdrant_client.async_qdrant_client import AsyncQdrantClient
 from qdrant_client.conversions import common_types as qdrant_types
 from qdrant_client.http import models
 
+from src.enums.skill_type import SkillType
+
 
 class VectorSearchRepository:
     def __init__(self, client: AsyncQdrantClient) -> None:
@@ -32,10 +34,32 @@ class VectorSearchRepository:
     async def delete_skill(self, skill_id: int) -> None:
         await self._client.delete(collection_name=self.collection_name, points_selector=[skill_id])
 
-    async def search(self, query_vector: list[float], limit: int = 10, offset: int = 0) -> qdrant_types.QueryResponse:
+    async def delete_skills(self, skill_ids: list[int]) -> None:
+        await self._client.delete(collection_name=self.collection_name, points_selector=skill_ids)
+
+    async def search(
+        self, query_vector: list[float], skilltype: SkillType, limit: int = 10, offset: int = 0
+    ) -> tuple[qdrant_types.QueryResponse, int]:
+        query_filter = models.Filter(
+            must=[
+                models.FieldCondition(
+                    key="type",
+                    match=models.MatchValue(value=skilltype.value),
+                )
+            ]
+        )
+        result_best_score = await self._client.query_points(
+            collection_name=self.collection_name, query=query_vector, query_filter=query_filter, limit=1
+        )
+        if not result_best_score:
+            return None
+        threshold = result_best_score.points[0].score * 0.89
+        total = await self._client.count(collection_name=self.collection_name, count_filter=query_filter)
         return await self._client.query_points(
             collection_name=self.collection_name,
             query=query_vector,
             limit=limit,
             offset=offset,
-        )
+            query_filter=query_filter,
+            score_threshold=threshold,
+        ), total
