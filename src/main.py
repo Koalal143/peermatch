9b91f609a1bd7b56397dda9ca1305f9900acc861
QuickFox43 <koalal1428@gmail.com>
2025-10-17 17:01:02 +0300

    fix(api): remove float project name

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ from src.core.config import settings
 
 
 app = FastAPI(
-    title=settings.project_name,
+    title="PeerMatch",
     description="API for PeerMatch",
     version="1.0.0",
     root_path="/api",
