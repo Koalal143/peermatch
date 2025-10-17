ccd1ce3d8974267bd5c6b70e060e6cdb30432021
QuickFox43 <koalal1428@gmail.com>
2025-10-17 18:56:41 +0300

    feat(exceptions): add auth exceptions

--- backend/src/exceptions/auth.py
+++ backend/src/exceptions/auth.py
@@ -3,3 +3,15 @@ from src.exceptions.base import BaseAppError
 
 class InvalidTokenError(BaseAppError):
     error_key = "invalid_refresh_token"
+
+
+class InvalidJWTError(BaseAppError):
+    error_key = "invalid_jwt"
+
+
+class JWTSignatureExpiredError(BaseAppError):
+    error_key = "jwt_signature_expired"
+
+
+class InactiveOrNotExistingUserError(BaseAppError):
+    error_key = "inactive_or_not_existing_user"
