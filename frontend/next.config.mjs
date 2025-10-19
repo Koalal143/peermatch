8475eff3cf161f131b2a6fed0b164b703a494f85
QuickFox43 <koalal1428@gmail.com>
2025-10-19 03:09:12 +0300

    feat(frontend): initialize nextjs app

---
+++ frontend/next.config.mjs
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    unoptimized: true,
+  },
+  output: 'standalone',
+  experimental: {
+    outputFileTracingRoot: undefined,
+  },
+}
+
+export default nextConfig
